cc17cc948feae987af928abc4d58464390b3adfb
Thierry1980 <thierrydebelder@gmail.com>
2023-11-06 16:39:54 +0100

    Add Responsive design and light/dark mode

--- 5_dark-kitchen-project/script.js
+++ 5_dark-kitchen-project/script.js
@@ -3,49 +3,57 @@ let menuItems = [
         image: "./images/bolognese.jpeg",
         name: "Pizza Bolognese",
         category: "Comfort Food",
-        description: "A delicious pizza covered in bolognese sauce."
+        description: "A delicious pizza covered in bolognese sauce.",
+        price: "14$"
     },
     {
         image: "./images/hamburger.jpeg",
         name: "Home-made Hamburger & Fries",
         category: "Comfort Food",
-        description: "Our glorious home-made hamburger made from grass-fed ground beef."
+        description: "Our glorious home-made hamburger made from grass-fed ground beef.",
+        price: "22$"
     },
     {
         image: "./images/vegan pizza.jpeg",
         name: "Vegan Pizza",
         category: "Vegan",
-        description: "Want a vegan alternative of our best-seller? Here is our vegan pizza."
+        description: "Want a vegan alternative of our best-seller? Here is our vegan pizza.",
+        price: "19$"
     },
     {
         image: "./images/steak.jpeg",
         name: "Steak & Fries",
         category: "Home Made",
-        description: "Our juicy steak with freshly cut fries."
+        description: "Our juicy steak with freshly cut fries.",
+        price: "28$"
     },
     {
         image: "./images/cesar.jpeg",
         name: "Cesar Salade",
         category: "Vegan",
-        description: "A delicious vegan version of a classic cesar salade."
+        description: "A delicious vegan version of a classic cesar salade.",
+        price: "15$"
     },
     {
         image: "./images/chicken nugget.jpeg",
         name: "Home-made Chicken Nuggets & Fries",
         category: "Home Made",
-        description: "Who doesn't love chicken nuggets, every childs favorite."
+        description: "Who doesn't love chicken nuggets, every childs favorite.",
+        price: "10$"
     },
     {
         image: "./images/fish.jpeg",
         name: "Fish & Chips",
         category: "Comfort Food",
-        description: "A British Classic, enough said."
+        description: "A British Classic, enough said.",
+        price: "24$"
     },
     {
         image: "./images/kebab.jpeg",
         name: "Turkish Kebab",
         category: "Comfort Food",
-        description: "You will think you are in Turkey when you smell and taste our delicious Kebab."
+        description: "You will think you are in Turkey when you smell and taste our delicious Kebab.",
+        price: "18$"
     },
 ]
 
@@ -61,7 +69,8 @@ const cardItems = () => {
         <img src="${data.image}" alt="Image">
         <h4 class="name">${data.name}</h4>
         <p class="category">Category: ${data.category}</p>
-        <p class="description">${data.description}</h4><br>
+        <p class="description">${data.description}</p>
+        <p class="price">${data.price}</p>
         <button class="orderButton">ORDER</button>
         </div>
         `
@@ -70,3 +79,32 @@ const cardItems = () => {
 }
 
 cardItems()
+
+let toggleBtn = document.getElementById("toggle-btn")
+let body = document.body
+let darkMode = localStorage.getItem('dark-mode')
+
+const enableDarkMode = () => {
+    toggleBtn.classList.replace('fa-sun', 'fa-moon');
+    body.classList.add('dark');
+    localStorage.setItem('dark-mode', 'enabled');
+}
+
+const disableDarkMode = () => {
+    toggleBtn.classList.replace('fa-moon', 'fa-sun');
+    body.classList.remove('dark');
+    localStorage.setItem('dark-mode', 'disabled');
+}
+
+if (darkMode === 'enabled') {
+    enableDarkMode();
+}
+
+toggleBtn.onclick = (e) => {
+    darkMode = localStorage.getItem('dark-mode')
+    if (darkMode === 'disabled') {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+}
